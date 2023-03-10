8009ef68bb3c10b20bd1257342e773922a3a1e3e
Antonio010R <antonroy009@gmail.com>
2023-03-10 15:53:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const getNotes = require("./notes.js");
+const chalk = require("chalk");
+const yargs = require("yargs");
+
+yargs.version("1.1.0");
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note!",
+  builder: {
+    title: {
+      description: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      description: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title : " + argv.title);
+    console.log("Body : " + argv.body);
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "List notes",
+  handler: function () {
+    console.log("List notes");
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Read a note");
+  },
+});
+
+yargs.parse();
---
+++ notes.js
@@ -0,0 +1,5 @@
+const getNotes = function () {
+  return "Your notes...";
+};
+
+const addNote = (module.exports = getNotes);
