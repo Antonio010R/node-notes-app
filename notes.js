aded8d56d20196064ab1806e09f129913f599a29
Antonio010R <antonroy009@gmail.com>
2023-03-13 16:58:56 +0530

    Creating add function

--- notes.js
+++ notes.js
@@ -1,5 +1,44 @@
+const fs = require("fs");
+const chalk = require("chalk");
+const { title } = require("process");
+
 const getNotes = function () {
   return "Your notes...";
 };
 
-const addNote = (module.exports = getNotes);
+const addNote = function (title, body) {
+  const notes = loadNotes();
+
+  const note = {
+    title: title,
+    body: body,
+  };
+
+  const filteredNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
+
+  if (filteredNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    console.log(chalk.green.bold("New Note added"));
+  } else {
+    console.log(chalk.red.bold("Note title taken"));
+  }
+};
+
+const saveNotes = function (notes) {
+  fs.writeFileSync("notes.json", JSON.stringify(notes));
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = { getNotes: getNotes, addNote: addNote };
